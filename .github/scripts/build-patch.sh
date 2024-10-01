fc7543076c61d9670709326dc02380f462fd1b1b
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-10-01 21:52:00 +0700

    ci: use flips

--- .github/scripts/build-patch.sh
+++ .github/scripts/build-patch.sh
@@ -2,4 +2,4 @@
 
 VERSION=`git describe --tags --abbrev=8 --dirty='!' | awk -F "-" '{print $1}'`
 cp ../pokecrystal/pokecrystal11.gbc baserom.gbc
-make -j$(nproc) pokeoctober-v.${VERSION}.ips
+make -j$(nproc) IPSPATCH="../flips/flips" IPSPATCH_COMMAND="--create" pokeoctober-v.${VERSION}.ips
